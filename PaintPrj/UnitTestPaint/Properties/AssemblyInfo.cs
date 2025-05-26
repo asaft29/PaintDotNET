6351593e09c3e60351b045127e207d18b750a3f3
alexandru-vezeteu <127968462+alexandru-vezeteu@users.noreply.github.com>
2025-05-26 13:14:27 +0300

    Add files via upload

---
+++ PaintPrj/UnitTestPaint/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPaint")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPaint")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f04de05-51c6-42ad-ad78-cf4df9baba04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PaintPrj/UnitTestPaint/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ImageIO;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace UnitTestPaint
+{
+    [TestClass]
+    public class Test
+    {
+        private PictureBox _canvas;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _canvas = new PictureBox();
+        }
+
+        // test pentru un canvas de dimensiuni mici
+        [TestMethod]
+        public void DLL_SaveSmallCanvas()
+        {
+            _canvas.Image = new Bitmap(100, 100);
+
+            try
+            {
+                ImageIO.ImageIO.Save(_canvas.Image);
+                Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"Eroare la salvarea unui canvas mic! : {e.Message}");
+            }
+        }
+    
+        // test pentru un canvas de dimensiuni mari
+        [TestMethod]
+        public void DLL_SaveBigCanvas()
+        {
+            _canvas.Image = new Bitmap(8000, 8000);
+
+            try
+            {
+                ImageIO.ImageIO.Save(_canvas.Image);
+                Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"Eroare la salvarea unui canvas mare! : {e.Message}");
+            }
+        }
+        
+        // test pentru o imagine transparenta sa nu fie erori
+        [TestMethod]
+        public void DLL_SaveTransparentCanvas()
+        {
+            _canvas.Image = new Bitmap(100, 100);
+            using (Graphics g = Graphics.FromImage(_canvas.Image))
+            {
+                g.Clear(Color.Transparent);
+            }
+
+            try
+            {
+                ImageIO.ImageIO.Save(_canvas.Image);
+                Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"Eroare la salvarea unei canvas transparent!: {e.Message}");
+            }
+        }
+        
+        // test pentru a vedea daca exceptia creeata in DLL este valida
+        [TestMethod]
+        [ExpectedException(typeof(ImageIO.ImageIOException))]
+        public void DLLSaveNullCanvas()
+        {
+            ImageIO.ImageIO.Save(null);
+        }
+
+        // test pentru cand se extrage corespunzator o imagine cu Load
+        [TestMethod]
+        public void DLL_LoadCorrectImage()
+        {
+            Random rand = new Random();
+
+            int width = rand.Next(1000);
+            int height = rand.Next(1000);
+
+            Image test = ImageIO.ImageIO.Load(width, height);
+
+            Assert.AreEqual(width, test.Width);
+            Assert.AreEqual(height, test.Height);
+
+        }
+
+        // testam atunci cand dam Load si nu incarcam nicio imagine
+        [TestMethod]
+        [ExpectedException(typeof(ImageIO.ImageIOException))]
+        public void DLL_LoadWrongImage()
+        {
+            Random rand = new Random();
+
+            Image test = ImageIO.ImageIO.Load(rand.Next(5000), rand.Next(5000));
+        
+        }
+    }
+}
